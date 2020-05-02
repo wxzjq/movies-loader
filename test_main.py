ea060794c1682bead050952d2aa659fed480be85
mlabouardy <mohamed@labouardy.com>
2020-05-02 15:55:56 +0200

    declarative pipeline

--- test_main.py
+++ test_main.py
@@ -1,6 +1,5 @@
 import unittest
 import json
-import xmlrunner
 
 class TestJSONLoaderMethods(unittest.TestCase):
     movies = []
@@ -20,5 +19,4 @@ class TestJSONLoaderMethods(unittest.TestCase):
         self.assertEqual(self.movies[0]['id'], 'tt0111161')
 
 if __name__ == '__main__':
-    runner = xmlrunner.XMLTestRunner(output='reports')
-    unittest.main(testRunner=runner)
+    unittest.main()
